849c3ec4e9272693299edacbc2b6aa64a7f9cbc4
AnActualEmerald <kevinliger13@gmail.com>
2021-08-08 02:32:53 -0400

    Got basic testing working

--- src/test.rs
+++ src/test.rs
@@ -1,12 +1,20 @@
-#[test]
-fn it_works() {
-    assert_eq!(2 + 2, 4);
-}
+use std::env;
 
+use crate::model::AnimeList;
 use crate::MALClient;
 use tokio_test::block_on;
 #[test]
-fn encryption_test() {
-    let client = block_on(MALClient::new("bruh", false, None));
-    client.decrypt_tokens("bruh");
+fn anime_list() {
+    let client = setup();
+    let expected =
+        serde_json::from_str::<AnimeList>(include_str!("test-data/anime_list.json")).unwrap();
+    let result = block_on(client.get_anime_list("one", Some(4))).expect("Error performing request");
+    let first = expected.data[0].node.id;
+    let res_first = result.data[0].node.id;
+    assert_eq!(first, res_first); //Really don't want to implement partial_eq for all these structs lol
+}
+
+fn setup() -> MALClient {
+    let token = env::var("MAL_TOKEN").expect("Accesss token not found in environment");
+    MALClient::with_access_token(&token)
 }
